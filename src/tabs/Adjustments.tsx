6c08e04fa17b19045528027215388377fbbd59cf
Kain Nhantumbo <nhantumbok@gmail.com>
2022-10-01 01:27:32 +0200

    refactor: removed filter component. docs: upgraded to version 1.0.2

--- src/tabs/Adjustments.tsx
+++ src/tabs/Adjustments.tsx
@@ -237,7 +237,7 @@ export default function Adjustments(): JSX.Element {
 								<div className='user-info'>
 									<h3>
 										<FaBug />
-										<span>Bug Tracker V1.0.1</span>
+										<span>Bug Tracker V1.0.2</span>
 									</h3>
 								</div>
 								<div className='user-info'>
